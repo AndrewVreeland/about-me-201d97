23ad696146439fc0bf013e48b166d4ecb9adb18e
Andrew Vreeland <andrewvreeland@gmail.com>
2023-02-08 22:15:57 -0500

    some nice new details

--- README.md
+++ README.md
@@ -1,11 +1,28 @@
 # LAB -02
 
 ## ABOUT ME
+      Meet Andrew Vreeland, a proud Floridian who has lived a life marked by diverse experiences and achievements.
+      Andrew, now 27 years old, has spent eight years in the military where he honed his leadership skills and served as
+      both an infantry assaultman and an avionics technician. During this time, he had the opportunity to travel
+      throughout Southeast Asia and live in different parts of the United States.
 
+      Recently, Andrew has embarked on a new chapter in his life by training to become a software engineer. With his
+      associates degree in avionics systems technology and a strong drive to learn, he is determined to master Java and
+      become a valuable asset in the tech industry.
+
+      Aside from his professional aspirations, Andrew is also a devoted husband to his beautiful wife Kelsy and a loving
+      father to their son Liam. They have a happy home filled with the love of their three dogs, Cooper,
+      Rosco, and Maybell.
+
+      Andrew's future goals include not only continuing to be a great husband and father, exploring the world
+      and making memories with his family. With his extensive leadership and teamwork skills, unwavering determination,
+      and a heart full of love, there is no doubt that Andrew Vreeland will continue to conquer new challenges and make
+      a positive impact in the world.
 ## Author: Andrew Vreeland
 
 ## Links and Resources
 
 ## Lighthouse Accessibility Report Score
-
+99
 ## Reflections and Comments
+Had some troubles with the while for loop today. I know this is something I need practice with because I run into similar issues everytime I use them. I feel with some extra repition things should smooth over.  Currently my page has 7 questions with interactive inputs from the user. There is a score tied to the questions that the user will recieve at the end of the journey. I have a photo of my family as the focal point of my page with the needed requirements listed below. 
--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 
+'use strict';
+let totalScore = 0;
 let userName = prompt('what is your name?');
 
 let qOne = prompt(`Hello ${userName}, welcome to my humbe abode! Are you ready to play a game? please answer the following with 'yes' or 'no'.`).toLowerCase();
@@ -14,34 +16,82 @@ while (qOne) {
     break;
   }
   else {
-    qOne = prompt('you need to write yes or no');
+    qOne = prompt('you need to write Yes or No');
   }
 }
 
 let qTwo = prompt('Is blue my favorite color?').toLowerCase();
 
 if (qTwo === ('no' || 'n')) {
+  totalScore++;
   alert(`Congrats ${userName}, green is my favorite color! `);
 }
 else alert('sorry, you got it wrong my favorite color is green');
 
 let qThree = prompt('Do I have 3 dogs?').toLowerCase();
 if (qThree === ('yes' || 'y')) {
-  'congrats you\'re correct I do have three dogs!';
+  totalScore++;
+  alert('congrats you\'re correct I do have three dogs!');
 }
 else alert('sorry you are wrong!');
 let qFour = prompt('Am I excited for this course?').toLowerCase();
 if (qFour === ('yes' || 'y')) {
+  totalScore++;
   alert('you\'re right! I am excited for this course!');
 }
 else alert('sorry you are wrong');
 
 let qFive = prompt('Do I live on the water?');
 if (qFive === ('no' || 'n')) {
-  alert('you\'re right, I unfortunately do not live on the water. :(');
+  totalScore++;
+  alert('you\'re right, I do not live on the water. :(');
 }
 else alert('shoot I wish I lived on the water!');
-alert(`Hope you had fun playing the game ${userName}.`);
+let numOfGuesses = 4;
+
+while (numOfGuesses) {
+  const userGuess = prompt('Guess a number between 1 and 10');
+  numOfGuesses--;
+  if (parseInt(userGuess) === 7) {
+    totalScore++;
+    alert('Congrats you picked the right number!');
+    break;
+  }
+  else if (parseInt(userGuess) > 7) {
+    alert('Your guess was too high');
+  }
+  else {
+    alert('Your guess was too low.');
+  }
+}
+const favBands = ['of mice and men', 'silverstein', 'alesana', 'dance gavin dance'];
+let totalGuesses = 6;
+
+
+while (totalGuesses) {
+  let userGuess = prompt('What are my favorite metal bands?').toLowerCase();
+  let correct = false;
+  for (let i = 0; i < favBands.length; i++) {
+    let favBand = favBands[i];
+    if (userGuess === favBand) {
+      totalScore++;
+      alert(`Congrats you guessed ${userGuess} correctly. Here are my top four favorite metal bands, ${favBands}`);
+      correct = true;
+      break;
+    }
+  }
+  totalGuesses--;
+  if (!correct) {
+    alert(`You have ${totalGuesses} total guesses remaining`);
+    alert(`your guess was ${userGuess} and was incorrect.`);
+  }
+  else { break; }
+
+}
+
+console.log(totalScore);
+
+alert(`Hope you had fun playing the game ${userName}. Your total score is ${totalScore}`);
 
 
 
